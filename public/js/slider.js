240ef539e73b7d443fb5054b19c7c0c3603647b5
priyanshuthakar24 <priyanshuthakar24@gmail.com>
2024-01-19 00:01:37 +0530

    styling of admin and user pannel

---
+++ public/js/slider.js
@@ -0,0 +1,45 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// auto play 
+var slideIndexs = 0;
+showSlidess();
+
+function showSlidess() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndexs++;
+  if (slideIndexs > slides.length) {
+    slideIndexs = 1;
+  }
+  slides[slideIndexs - 1].style.display = "block";
+  setTimeout(showSlidess, 5000);
+}
